f3b7a5a1ed077ccc44d74ac82e2250d5244f8306
Paolo Rios <paologhost14.pr@gmail.com>
2020-10-13 12:18:53 -0300

    agregue el AutoForm

--- auto/models.py
+++ auto/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Auto(models.Model):
     nombre = models.CharField(max_length=50,null=False,blank=False)
     precio = models.PositiveIntegerField()
-    stock = models.PositiveIntegerField()
     detalle = models.TextField(max_length=50)
+    anio = models.PositiveIntegerField()
+    fecha_ingreso = models.DateTimeField(auto_now=True)
