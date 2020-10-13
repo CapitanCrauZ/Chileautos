d6befb8fb7b15149f63a70edf1464b5c6cc1eff5
Paolo Rios <paologhost14.pr@gmail.com>
2020-10-13 11:58:42 -0300

    incorporación de models Probando Cosas

--- auto/models.py
+++ auto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Auto(models.Model):
+    nombre = models.CharField(max_length=50,null=False,blank=False)
+    precio = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    detalle = models.TextField(max_length=50)
